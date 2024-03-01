bbfa880453e5fb2dc858ca711f99a3d7aaa61eac
SnowDrive <luc.moralesr@gmail.com>
2024-03-01 09:37:11 -0300

    feat: add global types

---
+++ packages/core/src/types/global.d.ts
@@ -0,0 +1,5 @@
+/**
+ * This declaration is necessary to avoid the "TS2742" error when using curried functions.
+ * @see https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1885252911
+ */
+declare module 'ts-toolbelt' {}
