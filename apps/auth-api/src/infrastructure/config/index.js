da86b30e9d55a2dcd1eace644bbb03163cd01923
SnowDrive <luc.moralesr@gmail.com>
2024-03-02 11:12:00 -0300

    feat: add config file

---
+++ apps/auth-api/src/infrastructure/config/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+
+export const config = {
+	env: process.env.NODE_ENV,
+	server: {
+		port: process.env.PORT ?? 3002,
+		domainUrl: process.env.DOMAIN_URL,
+		cypherKey: process.env.CYPHER_KEY,
+	},
+	mongo: {
+		uri: process.env.MONGO_URI,
+		dbName: process.env.MONGO_DB_NAME,
+		timeout: 5000,
+	},
+	helix: {
+		botId: process.env.HELIX_BOT_ID,
+		clientId: process.env.HELIX_CLIENT_ID,
+		clientSecret: process.env.HELIX_CLIENT_SECRET,
+	},
+}
