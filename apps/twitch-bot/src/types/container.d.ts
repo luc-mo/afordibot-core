2167ac55a1685cadd052761ad0f2cc2adb7e2723
SnowDrive <luc.moralesr@gmail.com>
2024-03-13 10:54:11 -0300

    feat: add container types to handle jsdoc constructor params

---
+++ apps/twitch-bot/src/types/container.d.ts
@@ -0,0 +1,36 @@
+import type crypto from 'node:crypto'
+import type * as mongodb from 'mongodb'
+import type { config } from '@/infrastructure/config'
+
+import type { IdGenerator, Cipher } from '@afordibot/core'
+import type { CommandPicker } from '@/domain/services/command-picker'
+import type { CommandParser } from '@/domain/services/command-parser'
+
+import type { AuthProvider } from '@/infrastructure/irc/auth-provider'
+
+import type { MongoDbHandler } from '@/infrastructure/persistence/mongo/db-handler'
+import type { AuthorizationRepository } from '@/infrastructure/persistence/mongo/authorization/repository'
+import type { authorizationDocumentParser } from '@/infrastructure/persistence/mongo/authorization/document-parser'
+
+export interface Container {
+	// Libraries
+	crypto: typeof crypto
+	mongodb: typeof mongodb
+
+	// Config
+	config: typeof config
+
+	// Domain services
+	idGenerator: IdGenerator
+	cipher: Cipher
+	commandPicker: CommandPicker
+	commandParser: CommandParser
+
+	// Auth provider
+	authProvider: AuthProvider
+
+	// Persistence
+	dbHandler: MongoDbHandler
+	authorizationRepository: AuthorizationRepository
+	authorizationDocumentParser: typeof authorizationDocumentParser
+}
